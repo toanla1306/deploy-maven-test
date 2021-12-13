e2579c0b004d35d252fd630d1ddebe3700f379f1
toanla1306 <83157940+toanla1306@users.noreply.github.com>
2021-12-12 18:06:42 -0800

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,5 +1,5 @@
 def getVersionApp() {
-	sh(script: 'head -20 /var/lib/jenkins/workspace/simple-app/pom.xml | grep "<version>" | tail -1 | cut -d ">" -f2 | cut -d "<" -f1',returnStdout: true).trim()
+        sh(script: "head -20 ${env.WORKSPACE}/simple-app/pom.xml | grep '<version>' | tail -1 | cut -d '>' -f2 | cut -d '<' -f1",returnStdout: true).trim()
 }
 
 return this
