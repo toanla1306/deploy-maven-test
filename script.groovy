fe7a63eaa6eb8e463649d6cd9b273d0a813667b9
toanla1306 <83157940+toanla1306@users.noreply.github.com>
2021-12-12 18:28:42 -0800

    update some function use in Jenkinsfile

--- script.groovy
+++ script.groovy
@@ -2,4 +2,18 @@ def getVersionApp() {
         sh(script: "head -20 ${env.WORKSPACE}/pom.xml | grep '<version>' | tail -1 | cut -d '>' -f2 | cut -d '<' -f1",returnStdout: true).trim()
 }
 
+def getVersionBuildinDay() {
+        sh(script: "curl http://192.168.10.135:8081/service/rest/repository/browse/simpleapp-snapshot/org/springframework/samples/spring-petclinic/2.5.0-SNAPSHOT/ | grep 2.5.0-${now} | wc -l",returnStdout: true).trim()
+}
+
+def loginDockerwithNexus() {
+        withCredentials([usernamePassword(credentialsId:'dockerlogin', passwordVariable: 'password', usernameVariable: 'username')]) {
+                                                sh "docker login -u $username -p $password 192.168.10.135:8085"
+                                        }
+}
+
+def checkHealthDeploy() {
+        sh(script: 'curl -I 192.168.10.140:8085 | grep HTTP | cut -d " " -f2', returnStdout: true).trim()
+}
+
 return this
