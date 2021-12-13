d5575830a087f9c28166541adb7ab081458f1479
toanla1306 <83157940+toanla1306@users.noreply.github.com>
2021-12-12 18:08:01 -0800

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,5 +1,5 @@
 def getVersionApp() {
-        sh(script: "head -20 ${env.WORKSPACE}/simple-app/pom.xml | grep '<version>' | tail -1 | cut -d '>' -f2 | cut -d '<' -f1",returnStdout: true).trim()
+        sh(script: "head -20 ${env.WORKSPACE}/pom.xml | grep '<version>' | tail -1 | cut -d '>' -f2 | cut -d '<' -f1",returnStdout: true).trim()
 }
 
 return this
