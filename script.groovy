851c4bcd5bceba011845a63181d304cd22dc33a6
toanla1306 <83157940+toanla1306@users.noreply.github.com>
2021-12-13 21:13:28 -0500

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -11,9 +11,7 @@ def getVersionApp() {
 def getVersionBuildinDay() {
         now = getTime()
         version_app = getVersionApp()
-        echo now
-        echo version_app
-        sh(script: "curl http://nexus-repository.com:8081/service/rest/repository/browse/simpleapp-snapshot/org/springframework/samples/spring-petclinic/2.5.0-SNAPSHOT/ | grep 2.5.0-${now} | wc -l",returnStdout: true).trim()
+        sh(script: "curl http://nexus-repository.com:8081/service/rest/repository/browse/simpleapp-snapshot/org/springframework/samples/spring-petclinic/2.5.0-SNAPSHOT/ | grep ${version_app}-${now} | wc -l",returnStdout: true).trim()
 }
 
 def getTagsImageDocker() {
