9860af5ea2d282c01cbf186c1d6d013c34cac9e3
toanla1306 <83157940+toanla1306@users.noreply.github.com>
2022-01-05 01:08:20 -0800

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -21,7 +21,7 @@ def getTagsImageDocker() {
         return "${now}-${version_app}-${version_build_in_day}"
 }
 
-def sshReleaseVM(commandline, value_return_stdout=false, credentials_id='vmrelease'){
+def sshReleaseVM(commandline, value_return_stdout=false, credentials_id='releasevm1'){
         withCredentials([usernamePassword(credentialsId: credentials_id, passwordVariable: 'password', usernameVariable: 'username')]) {
                 sh(script: "sshpass -p ${password} ssh -o stricthostkeychecking=no ${username}@release-vm.com ${commandline}", returnStdout: value_return_stdout)
         }
